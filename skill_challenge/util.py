d3aec40395cea80b2e9d8f640e7d04b69d6edf25
Jan David Fischbach <mail@jan-dav.id>
2023-11-09 22:16:03 +0100

    Fix versions to a working state

--- skill_challenge/util.py
+++ skill_challenge/util.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['PDK', 'nm', 'TE_pol_fraction', 'te_fraction', 'cCrossSection', 'cMode', 'inner', 'cachedComputeModes']
 
-# %% ../nbs/00_util.ipynb 2
+# %% ../nbs/00_util.ipynb 3
 # util functions go here
 import numpy as np
 import gdsfactory.simulation.gtidy3d as gt
@@ -18,11 +18,11 @@ PDK.activate()
 
 nm = 1e-3
 
-# %% ../nbs/00_util.ipynb 3
-def TE_pol_fraction(self):
+# %% ../nbs/00_util.ipynb 5
+def TE_pol_fraction(mode):
         """TE polarization fraction according to Lumericals definition. (assuming a regular mesh)"""
-        Ex_sq = np.abs(self._data["Ex"])**2
-        Ey_sq = np.abs(self._data["Ey"])**2
+        Ex_sq = np.abs(mode._data["Ex"])**2
+        Ey_sq = np.abs(mode._data["Ey"])**2
         
         return np.sum(Ex_sq, axis=(0,1))/np.sum(Ex_sq+Ey_sq, axis=(0,1))
 
@@ -33,12 +33,12 @@ def te_fraction(mode: Mode) -> float:
     epsx = np.real(mode.cs.nx**2)
     e = np.sum(0.5 * eps0 * epsx * np.abs(mode.Ex) ** 2)
     h = np.sum(0.5 * mu0 * np.abs(mode.Hx) ** 2)
-    return float(e / (e + h))
-
-# %% ../nbs/00_util.ipynb 4
-setattr(gt.modes.Waveguide, 'TE_pol_fraction', property(TE_pol_fraction))
+    return np.real(e / (e + h))
 
 # %% ../nbs/00_util.ipynb 6
+setattr(gt.modes.Waveguide, 'TE_pol_fraction', property(TE_pol_fraction))
+
+# %% ../nbs/00_util.ipynb 8
 import meow as mw
 from functools import lru_cache
 import json
@@ -46,14 +46,14 @@ from hashlib import md5
 from meow import BaseModel, CrossSection, Mode
 from pydantic import Field
 
-# %% ../nbs/00_util.ipynb 7
+# %% ../nbs/00_util.ipynb 9
 def dict_to_hash(d: dict):
   """Converts a dictionary of distinctive properties into a hash using md5"""
   arr = np.frombuffer(md5(json.dumps(d).encode()).digest(), dtype=np.uint8)[-8:]
   idx = np.arange(arr.shape[0], dtype=np.int64)[::-1]
   return np.asarray(np.sum(arr * 255**idx), dtype=np.int_).item()
 
-# %% ../nbs/00_util.ipynb 8
+# %% ../nbs/00_util.ipynb 10
 class cCrossSection(BaseModel):
   cs: CrossSection = Field(
     description="the contained CrossSection"
@@ -80,7 +80,7 @@ class cMode(BaseModel):
   def __eq__(self, other):
     return hash(self) == hash(other)
 
-# %% ../nbs/00_util.ipynb 9
+# %% ../nbs/00_util.ipynb 11
 from functools import wraps
 def cached(func):
     func.cache = {}
@@ -93,7 +93,7 @@ def cached(func):
             return result   
     return wrapper
 
-# %% ../nbs/00_util.ipynb 10
+# %% ../nbs/00_util.ipynb 12
 @cached
 def inner(ccs, num_modes):
   return mw.compute_modes(ccs.cs, num_modes)
